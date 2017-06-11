c9d9bb9df37109c6a986a3fa1dfffed46911d315
vikash kumar <iit2016041@iiita.ac.in>
2017-06-11 23:13:42 +0530

    Create script.js

---
+++ JS_Basics/script.js
@@ -0,0 +1,35 @@
+
+// alert("Nice to meet you, " +userNameF + " " + userNameL);
+// console.log("Name =" + userNameF +userNameL);
+// console.log("age="+ age);
+
+
+
+
+// alert("Age calculator");
+// var age = prompt("what is your age ?");
+// var days = age * 365;
+// alert(age + " is roughly " + days+ "days");
+
+// var corr = 7;
+// var guess= Number( prompt("guess a number"));
+// if(guess === corr){
+// 	alert("Correct !!!");
+// }
+// else if (guess > corr){
+// 	alert("Too large");
+// 	//var guess= prompt("guess a number");
+// }
+// else {
+// 	alert("Too small");
+// 	//var guess= prompt("guess a number");
+// } 
+
+
+var output ="no";
+
+
+while (output != ("yes" || "yeah")){
+	output = prompt("Are you here ???");
+}
+alert("Finally you are here");
